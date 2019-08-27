12fa3e8389a6f90d312e1e664006df4d8391ecd5
Tom Snelling <tomsnelling8@gmail.com>
2019-08-27 17:56:04 +0100

    fix key deletion bug

--- index.js
+++ index.js
@@ -287,77 +287,82 @@ app.get('/search', (req, res) => {
 
 app.post('/remove', (req, res) => {
   if (req.body.fingerprint && req.body.message) {
-    db.get(`/${formatFingerprint(req.body.fingerprint)}`, (err, nodes) => {
-      if (!err) {
-        if (nodes[0]) {
-          openpgp.cleartext
-            .readArmored(req.body.message)
-            .then(message => {
-              openpgp.key
-                .readArmored(nodes[0].value.key)
-                .then(key => {
-                  const options = {
-                    message: message,
-                    publicKeys: key.keys
-                  }
-
-                  openpgp.verify(options).then(verified => {
-                    const valid = verified.signatures[0].valid
-                    if (valid) {
-                      db.del(
-                        `/${formatFingerprint(req.body.fingerprint)}`,
-                        err => {
-                          if (!err) {
-                            logger.info(
-                              `key ${formatFingerprint(
-                                req.body.fingerprint
-                              )} was removed successfully`
-                            )
-                            res.send(
-                              `<pre>Key ${formatFingerprint(
-                                req.body.fingerprint
-                              )} was removed successfully`
-                            )
-                          } else {
-                            logger.error(
-                              `there was an error removing key ${formatFingerprint(
-                                req.body.fingerprint
-                              )}`
-                            )
-                            res.sendStatus(500)
-                          }
-                        }
-                      )
-                    } else {
-                      logger.error(
-                        `user was not authorised to remove key ${formatFingerprint(
-                          req.body.fingerprint
-                        )}`
-                      )
-                      res.sendStatus(401)
+    db.get(
+      `/${formatFingerprint(req.body.fingerprint).slice(-16)}`,
+      (err, nodes) => {
+        if (!err) {
+          if (nodes[0]) {
+            openpgp.cleartext
+              .readArmored(req.body.message)
+              .then(message => {
+                openpgp.key
+                  .readArmored(nodes[0].value.key)
+                  .then(key => {
+                    const options = {
+                      message: message,
+                      publicKeys: key.keys
                     }
+
+                    openpgp.verify(options).then(verified => {
+                      const valid = verified.signatures[0].valid
+                      if (valid) {
+                        db.del(
+                          `/${formatFingerprint(req.body.fingerprint).slice(
+                            -16
+                          )}`,
+                          err => {
+                            if (!err) {
+                              logger.info(
+                                `key ${formatFingerprint(
+                                  req.body.fingerprint
+                                )} was removed successfully`
+                              )
+                              res.send(
+                                `<pre>Key ${formatFingerprint(
+                                  req.body.fingerprint
+                                )} was removed successfully`
+                              )
+                            } else {
+                              logger.error(
+                                `there was an error removing key ${formatFingerprint(
+                                  req.body.fingerprint
+                                )}`
+                              )
+                              res.sendStatus(500)
+                            }
+                          }
+                        )
+                      } else {
+                        logger.error(
+                          `user was not authorised to remove key ${formatFingerprint(
+                            req.body.fingerprint
+                          )}`
+                        )
+                        res.sendStatus(401)
+                      }
+                    })
                   })
-                })
-                .catch(err => {
-                  logger.error(`${err}`)
-                  res.sendStatus(500)
-                })
-            })
-            .catch(err => {
-              logger.error(`${err}`)
-              res.sendStatus(500)
-            })
+                  .catch(err => {
+                    logger.error(`${err}`)
+                    res.sendStatus(500)
+                  })
+              })
+              .catch(err => {
+                logger.error(`${err}`)
+                res.sendStatus(500)
+              })
+          } else {
+            logger.info(
+              `key ${formatFingerprint(req.body.fingerprint)} not found`
+            )
+            res.sendStatus(404)
+          }
         } else {
-          logger.info(
-            `key ${formatFingerprint(req.body.fingerprint)} not found`
-          )
-          res.sendStatus(404)
+          logger.error(`${err}`)
+          res.sendStatus(500)
         }
-      } else {
-        logger.error(`${err}`)
-        res.sendStatus(500)
       }
-    })
+    )
   } else {
     res.sendStatus(400)
   }
